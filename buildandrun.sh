90d8d5dd7fb8428c0b2c41c9e1b76b64c9bd374d
Dustin Firebaugh <dafirebaugh@gmail.com>
2020-12-30 01:50:40 -0500

    temp fix for weird frontend build issue

--- buildandrun.sh
+++ buildandrun.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# temporarily putting this here because there's a weird problem with docker building 
+#  when the ui build folders don't exist
+cd ui
+npm run rollup
+cd ..
+
 # point to a config file on the system
 # this directory is mapped as a volume
 export MEMBER_SERVER_CONFIG_FILE='/etc/hackrva/config.json'
