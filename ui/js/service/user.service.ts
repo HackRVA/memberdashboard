8914121440eeae86e940819df4ada0b17840fa8f
pacna <31336845+pacna@users.noreply.github.com>
2021-01-23 23:30:48 -0500

    remove proxy name in service

--- ui/js/service/user.service.ts
+++ ui/js/service/user.service.ts
@@ -5,21 +5,21 @@ export class UserService extends HTTPService {
   login(
     loginRequest: UserService.LoginRequest
   ): Observable<Response | { error: boolean; message: any }> {
-    return this.post("/edge/api/login", loginRequest);
+    return this.post("/api/login", loginRequest);
   }
 
   logout(): Observable<Response | { error: boolean; message: any }> {
-    return this.get("/edge/api/logout");
+    return this.get("/api/logout");
   }
 
   getUser(): Observable<Response | { error: boolean; message: any }> {
-    return this.get("/edge/api/user");
+    return this.get("/api/user");
   }
 
   registerUser(
     registerRequest: UserService.RegisterRequest
   ): Observable<Response | { error: boolean; message: any }> {
-    return this.post("/edge/api/register", registerRequest);
+    return this.post("/api/register", registerRequest);
   }
 }
 
