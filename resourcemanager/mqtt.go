8ae042119f4a8286ff998207c6bd1a32dc1bce21
Dustin Firebaugh <dafirebaugh@gmail.com>
2021-02-18 18:29:55 -0500

    fixing publish/subscribe issue that caused a recursive loop #52

--- resourcemanager/mqtt.go
+++ resourcemanager/mqtt.go
@@ -1,16 +1,31 @@
 package resourcemanager
 
 import (
+	"math/rand"
 	"os"
+	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	log "github.com/sirupsen/logrus"
 )
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randStringRunes(n int) string {
+	letterRunes := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
 // Subscribe - subscribe to an MQTT topic and pass in a messageHandler
 func Subscribe(topic string, handler mqtt.MessageHandler) {
 	opts := mqtt.NewClientOptions().AddBroker(os.Getenv("MQTT_BROKER_ADDRESS"))
-	opts.SetClientID("member-server")
+	opts.SetClientID("member-server-subscriber-" + randStringRunes(12))
 
 	opts.OnConnect = func(c mqtt.Client) {
 		if token := c.Subscribe(topic, 0, handler); token.Wait() && token.Error() != nil {
@@ -29,7 +44,7 @@ func Subscribe(topic string, handler mqtt.MessageHandler) {
 // Publish - publish to an MQTT topic
 func Publish(topic string, payload interface{}) {
 	opts := mqtt.NewClientOptions().AddBroker(os.Getenv("MQTT_BROKER_ADDRESS"))
-	opts.SetClientID("member-server")
+	opts.SetClientID("member-server-publisher-" + randStringRunes(12))
 
 	client := mqtt.NewClient(opts)
 	if token := client.Connect(); token.Wait() && token.Error() != nil {
