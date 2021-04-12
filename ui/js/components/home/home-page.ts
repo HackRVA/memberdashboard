5c707ae7801a9370d143730add7f44722fe03382
pacna <31336845+pacna@users.noreply.github.com>
2021-04-12 02:23:33 -0400

    add style file for home-page

--- ui/js/components/home/home-page.ts
+++ ui/js/components/home/home-page.ts
@@ -2,13 +2,13 @@
 import {
   LitElement,
   html,
-  css,
   customElement,
   TemplateResult,
   CSSResult,
 } from "lit-element";
 
 // membership
+import { homePageStyles } from "./styles/home-page-styles";
 import { UserService } from "../../service";
 import "../shared/card-element";
 import "../shared/register-form";
@@ -20,18 +20,8 @@ export class HomePage extends LitElement {
   isUserLogin: boolean = false;
   isRegister: boolean = false;
 
-  static get styles(): CSSResult {
-    return css`
-      .login-container {
-        display: grid;
-        justify-content: center;
-        padding: 36px;
-      }
-
-      .center {
-        text-align: center;
-      }
-    `;
+  static get styles(): CSSResult[] {
+    return [homePageStyles];
   }
 
   firstUpdated(): void {
---
+++ ui/js/components/home/styles/home-page-styles.ts
@@ -0,0 +1,13 @@
+import { css, CSSResult } from "lit-element";
+
+export const homePageStyles: CSSResult = css`
+  .login-container {
+    display: grid;
+    justify-content: center;
+    padding: 36px;
+  }
+
+  .center {
+    text-align: center;
+  }
+`;
