6b75f7f8da8da2ae4d4492ebf33ce8f09565b5a7
pacna <31336845+pacna@users.noreply.github.com>
2021-04-22 00:31:41 -0400

    Add : in Profile Struct

--- slack/slack.go
+++ slack/slack.go
@@ -13,7 +13,7 @@ import (
 
 type Profile struct {
 	DisplayName string `json:"display_name"`
-	RealName    string `json"real_name"`
+	RealName    string `json:"real_name"`
 	FirstName   string `json:"first_name"`
 	LastName    string `json:"last_name"`
 	Email       string `json:"email"`
