b6842b71a89c8ff90ebf51e415ce9353192bafde
Dustin Firebaugh <dafirebaugh@gmail.com>
2022-04-18 15:52:26 -0400

    fixing uneccessary logging (#170)

--- internal/services/member/member.go
+++ internal/services/member/member.go
@@ -2,15 +2,18 @@ package member
 
 import (
 	"errors"
+	"fmt"
 	"memberserver/internal/datastore"
 	"memberserver/internal/models"
 	"memberserver/internal/services/resourcemanager"
 	"memberserver/pkg/slack"
+	"strconv"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
 
-type Member struct {
+type memberService struct {
 	store           datastore.MemberStore
 	resourceManager resourcemanager.ResourceManager
 }
@@ -23,33 +26,35 @@ type MemberService interface {
 	AssignRFID(email string, rfid string) (models.Member, error)
 	GetTiers() []models.Tier
 	FindNonMembersOnSlack() []string
+	CancelStatusHandler(member models.Member, lastPayment models.Payment)
+	ActiveStatusHandler(member models.Member, lastPayment models.Payment)
 }
 
-func NewMemberService(store datastore.MemberStore, rm resourcemanager.ResourceManager) Member {
-	return Member{
+func NewMemberService(store datastore.MemberStore, rm resourcemanager.ResourceManager) memberService {
+	return memberService{
 		store:           store,
 		resourceManager: rm,
 	}
 }
 
-func (m Member) Add(newMember models.Member) (models.Member, error) {
+func (m memberService) Add(newMember models.Member) (models.Member, error) {
 	m.AssignRFID(newMember.Email, newMember.RFID)
 	return m.store.AddNewMember(newMember)
 }
 
-func (m Member) Get() []models.Member {
+func (m memberService) Get() []models.Member {
 	return m.store.GetMembers()
 }
 
-func (m Member) GetByEmail(email string) (models.Member, error) {
+func (m memberService) GetByEmail(email string) (models.Member, error) {
 	return m.store.GetMemberByEmail(email)
 }
 
-func (m Member) Update(member models.Member) error {
+func (m memberService) Update(member models.Member) error {
 	return m.store.UpdateMember(member)
 }
 
-func (m Member) AssignRFID(email string, rfid string) (models.Member, error) {
+func (m memberService) AssignRFID(email string, rfid string) (models.Member, error) {
 	if len(rfid) == 0 {
 		return models.Member{}, errors.New("not a valid rfid")
 	}
@@ -59,11 +64,11 @@ func (m Member) AssignRFID(email string, rfid string) (models.Member, error) {
 	return m.store.AssignRFID(email, rfid)
 }
 
-func (m Member) GetTiers() []models.Tier {
+func (m memberService) GetTiers() []models.Tier {
 	return m.store.GetTiers()
 }
 
-func (m Member) FindNonMembersOnSlack() []string {
+func (m memberService) FindNonMembersOnSlack() []string {
 	var nonMembers []string
 
 	users, err := slack.GetUsers()
@@ -93,4 +98,75 @@ func (m Member) FindNonMembersOnSlack() []string {
 		}
 	}
 	return nonMembers
+}
+
+func (ms memberService) CancelStatusHandler(m models.Member, lastPayment models.Payment) {
+	member := member{
+		store: ms.store,
+		model: m,
+	}
+
+	if member.paymentIsBeforeOneMonthAgo(lastPayment) {
+		if member.isActive() {
+			member.endGracePeriod()
+		}
+		member.setInactive()
+
+		return
+	}
+	member.notifyGracePeriod()
+}
+
+func (ms memberService) ActiveStatusHandler(m models.Member, lastPayment models.Payment) {
+	member := member{
+		store: ms.store,
+		model: m,
+	}
+	lastPaymentAmount, err := strconv.ParseFloat(lastPayment.Amount, 32)
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	member.setMemberLevel(lastPaymentAmount)
+}
+
+type member struct {
+	model models.Member
+	store datastore.MemberStore
+}
+
+func (m member) paymentIsBeforeOneMonthAgo(payment models.Payment) bool {
+	oneMonthAgo := (time.Hour * 24) * -30
+	return payment.Time.Before(time.Now().Add(oneMonthAgo))
+}
+
+func (m member) isActive() bool {
+	return m.model.Level == uint8(models.Standard) || m.model.Level == uint8(models.Classic) || m.model.Level == uint8(models.Premium)
+}
+
+func (m member) notifyGracePeriod() {
+	logrus.Infof("[scheduled-job] %s notify about being in grace period", m.model.Name)
+	go slack.Send(fmt.Sprintf("%s is in a grace period until their subscription ends", m.model.Name))
+}
+
+func (m member) endGracePeriod() {
+	logrus.Infof("[scheduled-job] %s notify about grace period ending", m.model.Name)
+	go slack.Send(fmt.Sprintf("%s grace period has ended. Setting membership level to inactive.", m.model.Name))
+}
+
+func (m member) setInactive() {
+	logrus.Infof("[scheduled-job] %s setting member to inactive", m.model.Name)
+	m.store.SetMemberLevel(m.model.ID, models.Inactive)
+}
+
+func (m member) setMemberLevel(lastPaymentAmount float64) {
+	if int64(lastPaymentAmount) == models.MemberLevelToAmount[models.Premium] {
+		m.store.SetMemberLevel(m.model.ID, models.Premium)
+		return
+	}
+	if int64(lastPaymentAmount) == models.MemberLevelToAmount[models.Classic] {
+		m.store.SetMemberLevel(m.model.ID, models.Classic)
+		return
+	}
+	m.store.SetMemberLevel(m.model.ID, models.Standard)
 }
