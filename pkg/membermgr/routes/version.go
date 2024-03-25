74faf5a863a6fa5ea59603ec4d59ebeef5fdd42c
pacna <31336845+pacna@users.noreply.github.com>
2024-03-25 14:17:37 -0400

    remove swagger comments

--- pkg/membermgr/routes/version.go
+++ pkg/membermgr/routes/version.go
@@ -9,19 +9,5 @@ type VersionHTTPHandler interface {
 }
 
 func (r Router) setupVersionRoutes(versionServer VersionHTTPHandler) {
-	// swagger:route GET /api/version version Version
-	//
-	//   Shows the current build's version information
-	//
-	//     Consumes:
-	//     - application/json
-	//
-	//     Produces:
-	//     - application/json
-	//
-	//     Schemes: http, https
-	//
-	//     Responses:
-	//       200: versionResponse
 	r.UnAuthedRouter.HandleFunc("/api/version", versionServer.ServeHTTP)
 }
